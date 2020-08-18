ea8155da873f574780b770385ff67bf4ea88687a
Melody <reid.eastin@gmail.com>
2020-08-18 10:58:16 -0500

    I added a new project

---
+++ aieProject2D1/main.cpp
@@ -0,0 +1,15 @@
+#include "BreakoutApp.h"
+
+int main() {
+	
+	// allocation
+	auto app = new aieProject2D1App();
+
+	// initialise and loop
+	app->run("AIE", 1280, 720, false);
+
+	// deallocation
+	delete app;
+
+	return 0;
+}
