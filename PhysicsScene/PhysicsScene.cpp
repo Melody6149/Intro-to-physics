28256272bdd14f23eef2192d63e590e2973a8b2f
Melody <reid.eastin@gmail.com>
2020-08-20 09:24:26 -0500

    updated things to 2019 and added remove actor and add actor

---
+++ PhysicsScene/PhysicsScene.cpp
@@ -0,0 +1,46 @@
+#include "PhysicsScene.h"
+PhysicsScene::PhysicsScene() : m_timeStep(0.01f), m_gravity(glm::vec2(0, 0))
+{
+}
+
+void PhysicsScene::addActor(PhysicsObject* actor)
+{
+	m_actors.push_back(actor);
+}
+
+void PhysicsScene::removeActor(PhysicsObject* actor)
+{
+	for (auto i = m_actors.begin(); i < m_actors.end(); i++)
+	{
+		if (*i == actor)
+		{
+			m_actors.erase(i);
+		}
+	}
+}
+
+void PhysicsScene::update(float deltaTime)
+{
+	 // update physics at a fixed time step
+
+	static float accumulatedTime = 0.0f;
+	accumulatedTime += deltaTime;
+
+	while (accumulatedTime >= m_timeStep)
+	{
+		for (auto pActor : m_actors)
+		{
+			pActor->fixedUpdate(m_gravity, m_timeStep);
+		}
+		
+		accumulatedTime -= m_timeStep;
+	}
+}
+
+void PhysicsScene::updateGizmos()
+{
+	for (auto pActor : m_actors)
+	{
+		pActor->makeGizmo();
+	}
+}
