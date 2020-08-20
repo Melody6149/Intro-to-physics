b810ccb08991ea10764ff8f9628a3b5f9beb78e8
Melody <reid.eastin@gmail.com>
2020-08-20 10:45:05 -0500

    I added new things

--- PhysicsScene/PhysicsSceneApp.cpp
+++ PhysicsScene/PhysicsSceneApp.cpp
@@ -2,6 +2,9 @@
 #include "Texture.h"
 #include "Font.h"
 #include "Input.h"
+#include"PhysicsScene.h"
+#include<Gizmos.h>
+
 
 PhysicsSceneApp::PhysicsSceneApp() {
 
@@ -12,12 +15,18 @@ PhysicsSceneApp::~PhysicsSceneApp() {
 }
 
 bool PhysicsSceneApp::startup() {
-	
+	// increase the 2D line cound to maximize the number of objects
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
+
 	m_2dRenderer = new aie::Renderer2D();
 
 	// TODO: remember to change this when redistributing a build!
 	// the following path would be used instead: "./font/consolas.ttf"
 	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+	m_physicsScene = new PhysicsScene();
+	m_physicsScene->setTimeStep(0.01f);
+
 
 	return true;
 }
@@ -33,6 +42,11 @@ void PhysicsSceneApp::update(float deltaTime) {
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
+	aie::Gizmos::clear();
+
+	m_physicsScene->update(deltaTime);
+	m_physicsScene->updateGizmos();
+
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
@@ -47,7 +61,12 @@ void PhysicsSceneApp::draw() {
 	m_2dRenderer->begin();
 
 	// draw your stuff here!
-	
+	static float aspectRatio = 16 / 9.0f;
+	aie::Gizmos::draw2D(glm::ortho<float>(
+		-100.0f, 100.0f,
+		-100.0f / aspectRatio, 100.0f / aspectRatio
+		));
+
 	// output some text, uses the last used colour
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
 
